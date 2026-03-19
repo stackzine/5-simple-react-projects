4deaf98746e6d21c1b9f44f630db15f3139a3c8f
Aniruddha Khartade <aniruddhakhartade@gmail.com>
2026-03-19 18:38:09 +0530

    responsiveness fix

--- 02-pomodoro-timer-app/src/components/PomodoroInfo.jsx
+++ 02-pomodoro-timer-app/src/components/PomodoroInfo.jsx
@@ -1,6 +1,11 @@
 function PomodoroInfo() {
   return (
-    <aside className="absolute w-80 right-20 top-1/2 -translate-y-1/2 text-sm text-gray-300">
+    <aside className="
+        w-full px-4 -mt-10 mb-6 text-sm text-center text-[#76789A] static
+
+        lg:absolute lg:w-80 lg:right-20 lg:top-1/2 lg:-translate-y-1/2 lg:text-left
+      "
+    >
       <p>
         A Pomodoro timer is a productivity tool, which breaks work 
         into 25-minute, highly focused, uninterrupted sessions (called "pomodoros") 
