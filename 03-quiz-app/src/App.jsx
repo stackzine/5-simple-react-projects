9f1918e042dee6b22c74680f07133d7d0a795f3b
Aniruddha Khartade <aniruddhakhartade@gmail.com>
2026-04-08 07:52:11 +0530

    add tailwindcss to project

--- 03-quiz-app/src/App.jsx
+++ 03-quiz-app/src/App.jsx
@@ -3,6 +3,6 @@ import './styles.css'
 
 export default function App() {
   return (
-    <h1>Hello React + Webpack</h1>
+    <h1 className="text-3xl font-bold underline">Hello React + Webpack</h1>
   )
 }
