5d1c054472af382150e7720458ef7ffa72e9d32d
Aniruddha Khartade <aniruddhakhartade@gmail.com>
2026-04-16 09:17:01 +0530

    husky only works at git root level

--- 03-quiz-app/src/App.test.jsx
+++ 03-quiz-app/src/App.test.jsx
@@ -4,7 +4,7 @@ import App from './App'
 test('renders heading', () => {
   render(<App />)
 
-  const heading = screen.getByText(/hello react \+ hello webpack/i)
+  const heading = screen.getByText(/hello react \+ webpack/i)
 
   expect(heading).toBeInTheDocument()
 })
